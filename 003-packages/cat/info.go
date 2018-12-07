02ff77c7f37914fabeb22ab42c9ff35703df463d
Kendra.Ware <Kendra.Ware@target.com>
2018-12-07 14:01:04 -0500

    Add package exercise

---
+++ 003-packages/cat/info.go
@@ -0,0 +1,13 @@
+package cat
+
+import "fmt"
+
+func FromCat() {
+
+	fmt.Println("Hello from cat")
+}
+
+func CatEats() {
+
+	fmt.Println("I eat cat food")
+}
---
+++ 003-packages/dog/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Kware22/go-programming/003-packages/cat"
+)
+
+func main() {
+	fmt.Println("Hello from dog!")
+
+	x := 1
+	if x == 1 {
+		cat.FromCat()
+	} else {
+		cat.CatEats()
+	}
+
+}
